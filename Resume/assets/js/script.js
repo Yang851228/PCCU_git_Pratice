ef829f0be0b744504cc5a5b4ae3f8224a226b193
Shuan <sam303643@gmail.com>
2024-07-29 15:17:24 +0800

    docs: 提交Resume

---
+++ Resume/assets/js/script.js
@@ -0,0 +1,114 @@
+$(function() {
+
+    $('.navbar-toggle').click(function() {
+        $(this).toggleClass('act');
+            if($(this).hasClass('act')) {
+                $('.main-menu').addClass('act');
+            }
+            else {
+                $('.main-menu').removeClass('act');
+            }
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 10
+    });
+
+	/* Progress bar */
+    var $section = $('.section-skills');
+    function loadDaBars() {
+	    $('.progress .progress-bar').progressbar({
+	        transition_delay: 500
+	    });
+    }
+    
+    $(document).bind('scroll', function(ev) {
+        var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+        if (scrollOffset > containerOffset) {
+            loadDaBars();
+            // unbind event not to load scrolsl again
+            $(document).unbind('scroll');
+        }
+    });
+
+    /* Counters  */
+    if ($(".section-counters .start").length>0) {
+        $(".section-counters .start").each(function() {
+            var stat_item = $(this),
+            offset = stat_item.offset().top;
+            $(window).scroll(function() {
+                if($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
+                    stat_item.addClass('counting');
+                    stat_item.countTo();
+                }
+            });
+        });
+    };
+
+	// another custom callback for counting to infinity
+	$('#infinity').data('countToOptions', {
+		onComplete: function (value) {
+		  count.call(this, {
+		    from: value,
+		    to: value + 1
+		  });
+		}
+	});
+
+	$('#infinity').each(count);
+
+	function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+
+    // Navigation overlay
+    var s = skrollr.init({
+            forceHeight: false,
+            smoothScrolling: false,
+            mobileDeceleration: 0.004,
+            mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
+    });
+    
+});
+
+//無法使用
+//聯絡我郵件寄送
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('contactForm');
+    if (form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+            var name = document.getElementById('sender_name').value;
+            var email = document.getElementById('sender_email').value;
+            var message = document.getElementById('sender_message').value;
+            
+            var mailtoLink = 'mailto:sam303643@gmail.com' +
+                '?subject=' + encodeURIComponent('聯絡表單') +
+                '&body=' + encodeURIComponent('姓名: ' + name + '\n電子郵件: ' + email + '\n訊息: ' + message);
+            
+            window.location.href = mailtoLink;
+            // 顯示成功提示
+            setTimeout(function() {
+                alert('郵件客戶端已成功打開。請在您的郵件客戶端中發送郵件以完成操作。');
+            }, 1000);
+        });
+    }
+});
